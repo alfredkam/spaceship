46a8f4cfb9300d166eab0327b381df1196fe89f4
alfredkam <akam@adcnx.com>
2015-03-15 00:23:25 -0700

    :art: Snap shot

--- dev/reactApp.jsx
+++ dev/reactApp.jsx
@@ -1,4 +1,6 @@
+// grid size n x m
 var grids = [5,5];
+// pixel size
 var size = 40;
 var gridProperties = ['x','o'];
 var matrix = [];
@@ -10,7 +12,8 @@ var body = document.getElementsByTagName("body")[0];
 
 var makeElement = function (m, n, content) {
     var obj = document.createElement('div');
-    obj.dataSet = m.toString() + n.toString();
+    obj.dataset.posM = m;
+    obj.dataset.posN = n;
     obj.innerHTML = content;
     obj.className = 'grid grid-' + content;
     obj.style.position = "absolute";
@@ -51,17 +54,74 @@ var appendGridToBody = function () {
     }
 };
 
+var getTileDirection = function (element, pos) {
+    var m = parseInt(element.style.top);
+    var n = parseInt(element.style.left);
+    var diffM = m - pos.y;
+    var diffN = n - pos.x;
+
+    if (Math.abs(diffM) > Math.abs(diffN)) {
+        // moving top or bottom
+        if (diffM < 0) {
+            return "bottom";
+        }
+        return "top";
+    } else {
+        // moving left or right
+        if (diffN < 0) {
+            return "right";
+        }
+        return "left";
+    }
+};
+
+var canChangeDirection = function (element, pos) {
+    var m = parseInt(element.dataset.posM) * size;
+    var n = parseInt(element.dataset.posN) * size;
+    var cY = parseInt(element.style.top);
+    var cX = parseInt(element.style.left);
+
+    if (m == cY && n == cX) {
+        return true;
+    }
+    return false;
+};
+
 var attachEvent = function (element) {
+    var start = true;
+    var prev = 0;
+    var currDirection = 0;
+    // need to switch to touch events later
     element.onmousedown = function () {
         document.onmousemove = mousemove = function (e) {
-            console.log(e.pageX - e.offsetX, e.pageY - e.offsetY);
             // now need to figure out if they moving left or right, and lock it down
-            // fix it down to relative of the finger
-            element.style.top = e.pageY + "px";
-            element.style.left = e.pageX + "px";
+            // the grid should be moving relative to finger instead of jumping to it
+            // need to adjust the x y relative to grid
+            var direction = getTileDirection(element, {
+                x: e.pageX,
+                y: e.pageY
+            });
+
+            if (!currDirection) {
+                currDirection = direction;
+            }
+            // check if its in grid then can change direction
+            if (canChangeDirection(element, {
+                x: e.pageX,
+                y: e.pageY
+            })) {
+                currDirection = direction;
+            }
+
+            if ((currDirection == 'left') || (currDirection == 'right')) {
+                element.style.left = e.pageX + "px";
+            } else {
+                element.style.top = e.pageY + "px";
+            }
         };
         // remove event
         document.onmouseup = mouseup = function (e) {
+            start = true;
             document.onmousemove = null;
             document.onmouseup = null;
         };
